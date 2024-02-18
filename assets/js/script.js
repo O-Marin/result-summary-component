a2fa52c5dedd9f07d5b02cdb70072ad7a773a469
O-marin <omarancb@gmail.com>
2024-02-18 09:41:34 -0300

    finished dinamic loading of json data, to do: fix summary section positions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,55 +9,65 @@ let jsonData = async () => {
 
 let jsonToObject = async () => {
   let statParagraphs = document.querySelectorAll('div#stats > p');
- 
-  /**
-   * let reactionParagraph = document.querySelector('#reaction');
-  let memoryParagraph = document.querySelector('#memory');
-  let verbalParagraph = document.querySelector('#verbal');
-  let visualParagraph = document.querySelector('#visual');
-   */
 
   let objeto = await jsonData();
   /**array with the images of the paragraphs */
   let imgs = [];
   statParagraphs.forEach(paragraph => imgs.push(paragraph.querySelector('img')));
-  console.log(imgs)
+
   /**array with the image reference */
   let imgSrc = []
-  objeto.forEach(element => imgSrc.push(element.icon));  
-  console.log(imgSrc);
+  objeto.forEach(element => imgSrc.push(element.icon));
+
   /**Pair up img element with img src in a single array */
   let icons = []
-
-  
-  /**to do, 
-   * crear un nuevo array que contenga el par elemento html IMG y el src del icono
-   * hacer lo mismo para los span dentro del parrafo, la categoria y el score
-   * darle id a los span para facilitar los selectores
-   *  */
- 
-  /**thought process
-   * I want to assign certain values to certain html elements
-   * one of the ideas I got was to create arrays of [html element, object value] to then assign them into the html
-   * another option could be creating new objects with those characteristics, but it could be more cumbersome.
-   */
-
 
 
   /**adding categories to array */
   let categories = [];
-  objeto.forEach(element => categories.push(element.category));  
-  console.log(categories);
+  objeto.forEach(element => categories.push(element.category));
+
   /**assigning score results into an array */
   let scores = []
   objeto.forEach(element => scores.push(element.score));
-  console.log(scores)
+
   /**seleccionar primer parrafo en el array que guarda los parrafos */
-  console.log(statParagraphs[0]);
-  console.log(statParagraphs[0].querySelectorAll(`p > span`))
-  
-  
-  //statParagraphs.forEach()
+
+  let reactionSpans = statParagraphs[0].querySelectorAll(`p > span`);
+  let spans = [];
+  /** */
+
+  statParagraphs.forEach(paragraph => {
+    //populating spans array with pairs of spans from each paragraph
+    spans.push(paragraph.querySelectorAll('p span'));
+  })
+
+spans[1][0].innerHTML 
+
+
+spans.forEach((span,index) => {
+
+ 
+  span.forEach((spa, index1)=>{
+
+    if(span[index1].innerHTML === ''){
+
+      span[0].innerHTML = categories[index];
+      span[1].innerHTML = `${scores[index]} <span style="display:inline-block;color: rgb(168, 158, 158);"> /100 </span>`;
+    }
+
+   
+    
+  })
+    
+})
+
+  //add images
+  for (let i in imgs) {
+    imgs[i].setAttribute('src', imgSrc[i]);
+  }
+
+ 
 }
 
 jsonToObject();
