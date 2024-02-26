e151f4593e081fb904a887bbbcbdd016e2e8e274
O-marin <omarancb@gmail.com>
2024-02-26 20:28:47 -0300

    result images upload

--- README-template.md
+++ README-template.md
@@ -16,7 +16,7 @@ This is a solution to the [Results summary component challenge on Frontend Mento
 - [Author](#author)
 - [Acknowledgments](#acknowledgments)
 
-**Note: Delete this note and update the table of contents based on what sections you keep.**
+
 
 ## Overview
 
@@ -30,7 +30,7 @@ Users should be able to:
 
 ### Screenshot
 
-![](./screenshot.jpg)
+![Mobile](./assets/results/mobile.jpg)	| ![Desktop](./assets/results/desktop.jpg)
 
 Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 let jsonData = async () => {
-  let test = await fetch('../../data.json').then(response => response.json()).then(
-    test => {
+  let test = await fetch("../../data.json")
+    .then((response) => response.json())
+    .then((test) => {
       return test;
-    }
-  );
+    });
   return test;
-}
+};
 
 let jsonToObject = async () => {
-
   let stats = await jsonData();
-  let statParagraphs = document.querySelectorAll('#stats div p');
-
+  let statParagraphs = document.querySelectorAll("#stats div p");
 
   //assigning json object values to arrays to iterate later.
   let icons = [];
@@ -23,48 +21,43 @@ let jsonToObject = async () => {
   let categoryElements = [];
   let scoreElements = [];
 
-  for(let stat of stats){
+  for (let stat of stats) {
     icons.push(stat.icon);
     categories.push(stat.category);
     scores.push(stat.score);
   }
-  console.log(icons,categories,scores);
+  console.log(icons, categories, scores);
 
   //paragraph elements
-  let elements = []
-  for(let element of statParagraphs){ 
+  let elements = [];
+  for (let element of statParagraphs) {
     elements.push(element.childNodes);
   }
-  
-  
-  for(let element in elements){ 
-    if(element % 2 === 0){
+
+  for (let element in elements) {
+    if (element % 2 === 0) {
       iconElements.push(elements[element][0]);
-      categoryElements.push (elements[element][1]);
-    }else if (element % 2 === 1){
+      categoryElements.push(elements[element][1]);
+    } else if (element % 2 === 1) {
       scoreElements.push(elements[element][0]);
+    }
   }
-  
-}
   console.log(iconElements);
   console.log(scoreElements);
   console.log(categoryElements);
 
-  for(let i in icons){
-    iconElements[i].setAttribute('src', icons[i]);
+  for (let i in icons) {
+    iconElements[i].setAttribute("src", icons[i]);
     categoryElements[i].innerHTML = categories[i];
     scoreElements[i].innerHTML = scores[i];
-    
   }
-  
+
   //result
-let total = 0;
-  for(let result of scores){
+  let total = 0;
+  for (let result of scores) {
     total += result;
   }
-  total = Math.round(total/scores.length)
-  document.querySelector('#result h1').innerHTML = total
-
-
-}
+  total = Math.round(total / scores.length);
+  document.querySelector("#result h1").innerHTML = total;
+};
 jsonToObject();
